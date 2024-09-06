7aa539b76e764d28c60158dd217bc8f218f0d601
FJY018399 <3206778990@qq.com>
2024-09-06 18:48:44 +0800

    deploy

--- deploy.ps1
+++ deploy.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = 'Stop'
 & fnm use --install-if-missing 20
 
 # 生成静态文件
-npm run build
+& npm run build
 
 # 进入生成的文件夹
 Set-Location .vuepress\dist
@@ -14,12 +14,12 @@ Set-Location .vuepress\dist
 # echo 'www.yourwebsite.com' > CNAME
 
 # 初始化 Git
-git init
-git add -A
-git commit -m 'deploy'
-git config user.name "FJY018399"
-git config user.email "3206778990@qq.com"
-git push -f https://github.com/FJY018399/FJY018399.github.io.git master
+& git init
+& git add -A
+& git commit -m 'deploy'
+& git config user.name "FJY018399"
+& git config user.email "3206778990@qq.com"
+& git push -f https://github.com/FJY018399/FJY018399.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
